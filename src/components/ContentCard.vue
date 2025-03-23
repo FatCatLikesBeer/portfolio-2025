1196910bb2b353777b34734df64281bd5e39e116
Billy Pamanian <itisbilly@gmail.com>
2025-03-23 16:48:00 -0700

    Image position indicator, left and right arrow

--- src/components/ContentCard.vue
+++ src/components/ContentCard.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import GitHub from "./icons/GitHub.vue";
+import LeftArrow from "./icons/LeftArrow.vue";
+import RightArrow from "./icons/RightArrow.vue";
+import ImagePositionIndicator from "./ImagePositionIndicator.vue";
 import type { Project } from "../data/projects.ts";
 
 const props = defineProps<{ project: Project }>();
 const imageIndex = ref(1);
 
-const imageLocation = `/public/screenshots/${props.project.id}/`;
+const imageLocation = `/screenshots/${props.project.id}/`;
 const imageCatlog = Array.from({ length: 3 }, (_, i) => `${imageLocation}/${i + 1}.png`);
 
 (function () {
@@ -16,9 +19,17 @@ const imageCatlog = Array.from({ length: 3 }, (_, i) => `${imageLocation}/${i + 
   });
 })();
 
-function handleClickImage() {
+function handleLeft() {
+  if (imageIndex.value <= 1) {
+    imageIndex.value = 3;
+  } else {
+    imageIndex.value = imageIndex.value - 1;
+  }
+}
+
+function handleRight() {
   if (imageIndex.value >= 3) {
-    imageIndex.value = 1
+    imageIndex.value = 1;
   } else {
     imageIndex.value = imageIndex.value + 1;
   }
@@ -27,14 +38,26 @@ function handleClickImage() {
 </script>
 
 <template>
-  <div class="border-amber-100 border-3 rounded-xl p-4 m-2">
-    <div v-for="(elem, i) in imageCatlog">
-      <div v-if="i + 1 === imageIndex" class="justify-items-center">
-        <img :src="elem" @click="handleClickImage" class="h-[80lvh] rounded-xl" />
+  <div class="border-pink-500 border-3 rounded-xl p-4 m-2">
+    <div class="flex flex-row items-center">
+      <LeftArrow :size="28" :callBack="handleLeft" />
+      <div v-for="(elem, i) in imageCatlog" class="flex flex-row">
+        <div v-if="i + 1 === imageIndex" class="justify-items-center">
+          <img :src="elem" class="h-[80lvh] rounded-xl" />
+        </div>
       </div>
+      <RightArrow :size="28" :callBack="handleRight" />
     </div>
+    <ImagePositionIndicator :location="imageIndex" />
     <h1 class="underline">{{ project.name }}</h1>
     <p class="sub-title">{{ project.description }}</p>
     <GitHub :url="project.github" :size="28" />
   </div>
 </template>
+
+<style>
+.image-select {
+  z-index: 9999;
+  height: 20px;
+}
+</style>
---
+++ src/components/ImagePositionIndicator.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const props = defineProps<{
+  location: number;
+}>();
+
+</script>
+
+<template>
+  <div class="flex flex-row justify-center">
+    <div :class="{ circle: true, selected: location === 1 }" />
+    <div :class="{ circle: true, selected: location === 2 }" />
+    <div :class="{ circle: true, selected: location === 3 }" />
+  </div>
+</template>
+
+<style>
+.circle {
+  width: 4px;
+  height: 4px;
+  margin: 0 4px;
+  border-radius: 100%;
+  border: grey solid 4px;
+}
+
+.selected {
+  border: white solid 4px;
+}
+</style>
