74bd020f825004dae12a227696937735d3d3da96
Billy Pamanian <itisbilly@gmail.com>
2025-03-26 02:04:20 -0700

    Fixed typos

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,7 @@
 <template>
   <footer class="flex flex-col justify-center text-neutral-400 text-sm m-12 mt-18">
     <p class="m-auto">Created by Billy</p>
-    <p class="m-auto">Written in <a href="https://vuejs.org/" target="_blank">Vue 3 </a>Composition API</p>
+    <p class="m-auto">Written with <a href="https://vuejs.org/" target="_blank">Vue 3 </a>Composition API</p>
     <p class="m-auto">All Icons by <a href="https://icons.getbootstrap.com/" target="_blank">Bootstrap</a></p>
   </footer>
 </template>
